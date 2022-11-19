85560e7aa1a6e6dd1281ebcc693d172f5e0e22a4
ahmet sakrak <ahmetsakrak776@gmail.com>
2022-11-19 17:11:14 +0300

    configurated

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const express = require("express");
 const DBConnect  = require("./config/db");
 const dotenv = require("dotenv").config();
@@ -16,6 +17,11 @@ server.use(express.urlencoded({extended:false}));
 server.use("/api/users", require("./routes/userR"));
 server.use("/api/collections", require("./routes/collectionR"));
 
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/build")))
+    app.get("*", (req, res)=>res.sendFile(path.resolve(__dirname,"../","frontend","build","index.html")))
+}
 
 server.use(errorHandler);
 server.listen(port, ()=>console.log("Hello World!"));
